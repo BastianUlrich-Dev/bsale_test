6aab99a669fb7434ecf97566066231d316dc4e99
Marmota-hub <bastianjorquera8@gmail.com>
2022-03-06 21:50:58 -0300

    se corrigieron errores

--- src/database.js
+++ src/database.js
@@ -1,14 +1,20 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
+    // host: 'localhost',
     host: 'localhost',
     // host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
     user: 'root',
+    // user: 'root',
     // user: 'bsale_test',
     password: '',
+    // password: '',
     // password: 'bsale_test',
     database: 'bsale_test'
+    // database: 'bsale_test'
 })
+
+// bb2f8f51dfa068:72128506@us-cdbr-east-05.cleardb.net/heroku_77525fe8d842d38
 
 mysqlConnection.connect(function (err){
     if(err){
