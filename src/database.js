0a5a32e5565594a1a93482cde3b432b372511dcb
Marmota-hub <bastianjorquera8@gmail.com>
2022-03-07 12:30:11 -0300

    make it better

--- src/database.js
+++ src/database.js
@@ -1,17 +1,10 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    // host: 'localhost',
     host: 'us-cdbr-east-05.cleardb.net',
-    // host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
     user: 'bb2f8f51dfa068',
-    // user: 'root',
-    // user: 'bsale_test',
     password: '72128506',
-    // password: '',
-    // password: 'bsale_test',
     database: 'heroku_77525fe8d842d38'
-    // database: 'bsale_test'
 })
 
 // bb2f8f51dfa068:72128506@us-cdbr-east-05.cleardb.net/heroku_77525fe8d842d38
