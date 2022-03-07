c80d159c4c367e0bb94cc658e531f4b44a271fa4
Marmota-hub <bastianjorquera8@gmail.com>
2022-03-06 22:17:18 -0300

    Add config db

--- src/database.js
+++ src/database.js
@@ -2,15 +2,15 @@ const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
     // host: 'localhost',
-    host: 'localhost',
+    host: 'us-cdbr-east-05.cleardb.net',
     // host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
-    user: 'root',
+    user: 'bb2f8f51dfa068',
     // user: 'root',
     // user: 'bsale_test',
-    password: '',
+    password: '72128506',
     // password: '',
     // password: 'bsale_test',
-    database: 'bsale_test'
+    database: 'heroku_77525fe8d842d38'
     // database: 'bsale_test'
 })
 
